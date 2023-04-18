0657174a0fd1492c9fdc164116769b55e46af0f7
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-04-18 22:45:20 +0800

    fix text

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   const meta = {
     title: 'Munkhjin | Portfolio',
-    description: 'Munkhjin`s Portfolio website',
+    description: 'Munkhjin`s Portfolio Website',
   };
 
   return (
