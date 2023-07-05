02c719554ee0c96ca0dc29e6c7d8816eb2797113
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-07-05 16:07:59 +0800

    disable lang

--- src/app/[lang]/dictionaries.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only';
-
-const dictionaries = {
-  en: () => import('./dictionaries/en.json').then((module) => module.default),
-  mn: () => import('./dictionaries/mn.json').then((module) => module.default),
-};
-
-export const getDictionary = async (locale) => dictionaries[locale]();
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1 className='text-4xl pb-4'>Мөнхжин Бадамхатан</h1>
+      <article className='text-2xl'>
+        Сайн байна уу? Намайг Мөнхжин гэдэг. Би програм хангамжийн инжинер мэргэжилтэй.
+      </article>
+    </div>
+  );
+}
