1b490e08c41ea9b92d287da1a69c0a170eef7ea2
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-07-11 15:42:54 +0800

    add payment info

--- src/components/NextJsLesson.tsx
+++ src/components/NextJsLesson.tsx
@@ -19,6 +19,17 @@ const Lesson: FunctionComponent<LessonProps> = () => {
         <p className='text-xl'>
           Үнэ: <span className='text-red-400'>29,900₮</span> <br />
           2023.08.01-ээс эхлэн үнэ <span className='text-red-400'>49,900₮</span> болно.
+          <br />
+        </p>
+        <p>
+          Төлбөрийг "Хас" банкны 5001381903 (Мөнхжин) дугаарт хийнэ. Гүйлгээний утга дээр Discord username-ээ бичнэ.
+        </p>
+        <p>
+          Жич:{' '}
+          <a href='https://discord.gg/QMR6Q2Tud4' target='_blank' className='underline'>
+            Discord сервер
+          </a>
+          -т заавал нэгдэх
         </p>
       </CardContent>
       <CardFooter className='justify-center'>
